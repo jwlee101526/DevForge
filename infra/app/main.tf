22b2597eb1d3a6059830e3ea35c3da5ee5395038
jwlee101526 <jwlee101526@kakao.com>
2026-08-13 20:09:46 +0900

    fix: update terraform health check path to /api/v1/quiz/providers

--- infra/app/main.tf
+++ infra/app/main.tf
@@ -66,7 +66,7 @@ resource "azurerm_linux_web_app" "main" {
     always_on                         = true
     ftps_state                        = "Disabled"
     health_check_eviction_time_in_min = 2
-    health_check_path                 = "/api/quiz/providers"
+    health_check_path                 = "/api/v1/quiz/providers"
     minimum_tls_version               = "1.2"
     scm_minimum_tls_version           = "1.2"
     use_32_bit_worker                 = false
